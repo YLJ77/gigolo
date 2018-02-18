bf6a09a8a18cd65a7dc21fe5479d1b870c79ecd7
ylj <564296557@qq.com>
2018-02-18 14:07:39 +0800

    add database

---
+++ client/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-toolbar class="cyan" fixed>
+        <v-toobar-title class="mr-4">
+            TabTracker
+        </v-toobar-title>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        data() {
+        
+        }
+    }
+</script>
+
+<style scoped></style>
